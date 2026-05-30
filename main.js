b4f8d8ee2548cca0965ce4f40211fd01f570a814
matheushenriquereiter <mathenriquereiter@gmail.com>
2026-05-29 21:39:29 -0300

    Added pokémon input suggestion list

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 const adicionarPokemonForm = document.getElementById("adicionar-pokemon-form");
 const cardsPokemon = document.getElementById("cards-pokemon");
 const erroMensagem = document.getElementById("erro-mensagem");
+const nomePokemonInput = document.getElementById("nome-pokemon-input");
+const datalist = document.getElementById("sugestoes-pokemon");
+
+const nomesPokemonsAdicionados = [];
+
+async function carregarSugestoes() {
+  const pokemons = await obterPokemons();
+
+  for (let i = 0; i < pokemons.count; i++) {
+    const pokemon = pokemons.results[i];
+
+    const nomePokemon = pokemon.name;
+
+    const option = document.createElement("option");
+    option.value = nomePokemon;
+    datalist.appendChild(option);
+  }
+}
+carregarSugestoes();
 
 adicionarPokemonForm.addEventListener("submit", async function (event) {
   event.preventDefault();
@@ -8,6 +27,11 @@ adicionarPokemonForm.addEventListener("submit", async function (event) {
   const nomePokemon = adicionarPokemonForm.nomePokemon.value
     .trim()
     .toLowerCase();
+
+  if (nomesPokemonsAdicionados.includes(nomePokemon)) {
+    erroMensagem.innerHTML = "Pokémon já adicionado";
+    return;
+  }
 
   if (nomePokemon === "") {
     erroMensagem.innerHTML = "Digite um nome válido";
@@ -28,6 +52,7 @@ adicionarPokemonForm.addEventListener("submit", async function (event) {
     </div>
   `;
 
+  nomesPokemonsAdicionados.push(nomePokemon);
   cardsPokemon.innerHTML += cardPokemon;
   erroMensagem.innerHTML = "";
 });
@@ -52,7 +77,6 @@ async function obterPokemon(nomePokemon) {
   }
 }
 
-/*
 async function obterPokemons() {
   const rotaApi = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
 
@@ -72,4 +96,3 @@ async function obterPokemons() {
     return null;
   }
 }
-*/
